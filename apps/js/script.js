b2a26c8894a4a06186e5dc50c0ee699154d18ded
sonishn213 <73402777+sonishn213@users.noreply.github.com>
2021-06-16 22:12:09 +0530

    Add files via upload

--- apps/js/script.js
+++ apps/js/script.js
@@ -1,1 +1,13 @@
-console.log("HEllo im JS");
+window.addEventListener("load", function () {
+  function changeimg(x) {
+    if (x.matches) {
+      // If media query matches
+      let imgcls = document.getElementsByClassName("imgs")[0];
+      imgcls.src = "./images/image-header-mobile.jpg";
+    }
+  }
+
+  var x = window.matchMedia("(max-width: 376px)");
+  changeimg(x); // Call listener function at run time
+  x.addListener(changeimg);
+});
